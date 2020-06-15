58595265b37289b7bcde774625d552ced0cd0887
SanghyukChun <sanghyuk.chun@gmail.com>
2020-06-15 17:27:45 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='adamp',
+    version='0.1',
+    author='NAVER Corp.',
+    description='AdamP optimizer: Slowing Down the Weight Norm Increase in Momentum-based Optimizers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/clovaai/AdamP',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
